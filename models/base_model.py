a437fa521b0a022b9366ea782340f0be774b70e9
FranQ <frankchristopherquispe@gmail.com>
2022-06-29 00:02:39 +0000

    base_model.py: adding basic structure

---
+++ models/base_model.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+class BaseModel:
+
+    def __init__(self):
+        self.id =
+        self.created_at =
+        self.updated_at =
+
+    def __str__(self):
+        pass
+
+    def save(self):
+        pass
+
+    def to_dict(self):
+        pass
