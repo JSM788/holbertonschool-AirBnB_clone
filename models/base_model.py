cb8b530b1b06fa4c41567f74c45ce68e67dca1c9
JSM788 <salinasjuan788@gmail.com>
2022-06-29 01:28:04 -0500

    BaseModel->task4 - adding conditions to kwargs arguments

--- models/base_model.py
+++ models/base_model.py
@@ -4,15 +4,22 @@ from uuid import uuid4
 
 
 class BaseModel:
-    
+
     def __init__(self, *args, **kwargs):
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
 
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs is not None:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__.update({k: datetime.strptime(v, t_format)})
+                elif k != "__class__":
+                    self.__dict__.update({k: v})
+
     def __str__(self):
         return (f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}')
-
 
     def save(self):
         self.updated_at = datetime.today()
@@ -20,6 +27,6 @@ class BaseModel:
     def to_dict(self):
         new_dic = self.__dict__.copy()
         new_dic['__class__'] = self.__class__.__name__
-        new_dic.update({'created_at' : self.created_at.isoformat()})
-        new_dic.update({'updated_at' : self.updated_at.isoformat()})
+        new_dic.update({'created_at': self.created_at.isoformat()})
+        new_dic.update({'updated_at': self.updated_at.isoformat()})
         return new_dic
