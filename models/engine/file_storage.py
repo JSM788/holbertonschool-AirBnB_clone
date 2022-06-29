b473b019a4e33a92400f0b2985346bce102db9a9
JSM788 <salinasjuan788@gmail.com>
2022-06-29 15:36:57 -0500

    BaseModel->task5 - adding methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+import file_storage
+
+class FileStorage:
+
+    def __init__(self):
+        self.__file_path
+        self.__objects
+        
+    def all(self):
+        return self.__dict__.__objects
+
+    def new(self, obj):
+        pass
+
+    def save(self):
+        pass
+
+    def reload(self):
+        self.storagels
 
