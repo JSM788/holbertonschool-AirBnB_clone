a066ebc5384b54ed937b8f44e79b7af056ec0ea2
JSM788 <salinasjuan788@gmail.com>
2022-07-01 22:11:11 -0500

    base_model.py->task3 - adding tests in base_model.py

---
+++ tests/test_base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestHBNBCommand_prompting(unittest.TestCase):
+    """Unittests for testing prompting of the HBNB command interpreter."""
+
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
