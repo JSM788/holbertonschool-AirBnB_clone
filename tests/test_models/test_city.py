ba3226104205c73e299fe6948a0df058d015d7d7
JSM788 <salinasjuan788@gmail.com>
2022-07-03 15:38:38 -0500

    adding tests of the classes

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+    TestCity module
+"""
+import unittest
+from models.city import City
+
+
+class TestCity(unittest.TestCase):
+    """
+        TestCity class
+    """
+
+    def test_should_create_intance_of_city(self):
+        """
+            Test instance creation
+        """
+        city = City()
+        self.assertIsInstance(city, City)
+
+    def test_should_create_instance_variable(self):
+        """
+            Test variable
+        """
+        city = City()
+        self.assertIsInstance(city.state_id, str)
+        self.assertIsInstance(city.name, str)
